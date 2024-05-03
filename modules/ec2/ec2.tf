5f0d37d3912c7740dc78c1ddbd23116c76791411
jpicken <jamesapicken@gmail.com>
2024-05-03 17:07:42 +0100

    updated entry script

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -31,7 +31,14 @@ resource "aws_instance" "server" {
     key_name = "testing_ssh_key"
     user_data = <<-EOF
         #!/bin/bash
-        echo "Hello from inline script!"
+        sudo yum -y update && sudo yum -y install httpd
+        sudo systemctl start httpd && sudo systemctl enable httpd 
+        sudo echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+
+        sudo yum -y install docker
+        sudo systemctl start docker
+        sudo usermod -aG docker ec2-user
+        sudo docker container run -d -p 8080:80 nginx
     EOF
     tags = {
         "Name" = "My EC2 Intance - Amazon Linux 2"
